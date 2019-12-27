de57c2e7f65cb25c3327160229325d67fd63e80f
linyongkun <1843515130@qq.com>
2019-12-27 16:03:00 +0800

    尝试配置短融器失败

--- business/business-note/src/main/java/com/seaweed/business/note/BusinessNoteApplication.java
+++ business/business-note/src/main/java/com/seaweed/business/note/BusinessNoteApplication.java
@@ -3,12 +3,14 @@ package com.seaweed.business.note;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author lyk
  * 业务模块启动类
  */
+@EnableHystrix
 @EnableEurekaClient
 @EnableFeignClients("com.seaweed")
 @SpringBootApplication
---
+++ core-base-api/src/main/java/com/seaweed/core/base/api/rpc/hystric/UserApiHystric.java
@@ -0,0 +1,18 @@
+//package com.seaweed.core.base.api.rpc.hystric;
+//
+//import com.seaweed.common.mybatis.tool.result.Result;
+//import com.seaweed.core.base.api.rpc.UserApi;
+//import org.springframework.stereotype.Component;
+//
+///**
+// * 用户外部调用api断路器
+// *
+// * @author lyk
+// */
+//@Component
+//public class UserApiHystric implements UserApi {
+//    @Override
+//    public Result<String> getName() {
+//        return Result.fail("get name fails");
+//    }
+//}
