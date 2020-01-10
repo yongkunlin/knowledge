571b1cbdf69df72a04403d965aa9a29cd1031b8b
linyongkun <1843515130@qq.com>
2020-01-10 15:14:06 +0800

    调整包结构 集成gateway动态路由

---
+++ gateway/src/main/java/com/seaweed/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package com.seaweed.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * 网关启动类
+ *
+ * @author lyk
+ */
+
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
